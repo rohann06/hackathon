3b1d929aeb9e41a8ca7d43db86fe8eb072d418bc
Sneha Mishra <itm2017004@iiita.ac.in>
2023-04-14 17:19:26 +0900

    setup aptos wallet adapter for connecting - martian, blocto, petra

--- components/Section/AptosWalletConnect/index.js
+++ components/Section/AptosWalletConnect/index.js
@@ -1,48 +1,37 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { useCallback, useMemo, useState } from "react";
+import { useState } from "react";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+
 
 const AptosWalletConnect = () => {
-  const router = useRouter();
-  const [address, setAddress] = useState(null);
+    const router = useRouter();
+    const [address, setAddress] = useState(null);
 
-  const isAptosDefined = useMemo(
-    () => (typeof window !== "undefined" ? Boolean(window?.aptos) : false),
-    []
-  );
+    const {
+        connect,
+        account,
+        network,
+        connected,
+        disconnect,
+        wallet,
+        wallets,
+        signAndSubmitTransaction,
+        signTransaction,
+        signMessage,
+        signMessageAndVerify,
+    } = useWallet();
 
-  // callback when a user clicks the "connect" button
-  const connectWallet = useCallback(async () => {
-    console.log('calling connectwallet')
-    try {
-      if (isAptosDefined) {
-        await window.aptos.connect();
-        const account = await window.aptos.account();
-        console.log('account :', account.address)
-        setAddress(account.address);
-      }
-    } catch (e) {
-      console.error("error: " , e);
-    }
-  }, [isAptosDefined]);
-
-  return (
-    <div className=" flex justify-end  gap-5 mx-6">
-      <button
-        onClick={() => router.push("/create")}
-        className=" bg-blue-600 text-white rounded-xl font-semibold text-[14px] lg:text-[16px] px-8 lg:px-10 py-2"
-      >
-        + Create
-      </button>
-      <button
-        // disabled={!isAptosDefined}
-        onClick={connectWallet}
-        className=" bg-blue-600 text-white rounded-xl font-semibold text-[14px] lg:text-[16px] px-8 lg:px-10 py-2 cursor-pointer"
-      >
-        {!address ? "Connect Wallet" : address}
-      </button>
-    </div>
-  );
+    return ( <
+        div className = " flex justify-end  gap-5 mx-6" >
+        <
+        WalletSelector / >
+        <
+        /
+        div >
+    );
 };
 
 export default AptosWalletConnect;
--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,48 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import {
-  LivepeerConfig,
-  createReactClient,
-  studioProvider,
+    LivepeerConfig,
+    createReactClient,
+    studioProvider,
 } from "@livepeer/react";
 import { AptosClient } from "aptos";
 import { createContext, useMemo } from "react";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { BloctoWallet } from "@blocto/aptos-wallet-adapter-plugin";
+import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 
 export const AptosContext = createContext(null);
 
 const livepeerClient = createReactClient({
-  provider: studioProvider({
-    apiKey: process.env.NEXT_PUBLIC_STUDIO_API_KEY,
-  }),
+    provider: studioProvider({
+        apiKey: process.env.NEXT_PUBLIC_STUDIO_API_KEY,
+    }),
 });
 
 export default function App({ Component, pageProps }) {
-  const aptosClient = useMemo(
-    () => new AptosClient("https://fullnode.devnet.aptoslabs.com/v1"),
-    []
-  );
-  return (
-    <Layout>
-      <AptosContext.Provider value={aptosClient}>
-        <LivepeerConfig client={livepeerClient}>
-          <Component {...pageProps} />
-        </LivepeerConfig>
-      </AptosContext.Provider>
-    </Layout>
-  );
+    const aptosClient = useMemo(
+        () => new AptosClient("https://fullnode.devnet.aptoslabs.com/v1"), []
+    );
+
+    const wallets = [new PetraWallet(), new MartianWallet()];
+
+    return ( <
+        AptosWalletAdapterProvider plugins = { wallets }
+        autoConnect = { true } >
+        <
+        Layout >
+
+        <
+        AptosContext.Provider value = { aptosClient } >
+        <
+        LivepeerConfig client = { livepeerClient } >
+        <
+        Component {...pageProps }
+        /> < /
+        LivepeerConfig > <
+        /AptosContext.Provider> < /
+        Layout > <
+        /AptosWalletAdapterProvider>
+    );
 }
